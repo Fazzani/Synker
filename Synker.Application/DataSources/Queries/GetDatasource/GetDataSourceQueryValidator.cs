e517b9d762bf2daa70a529559fd1901f6aa93d66
Heni Fazzani <heni.fazzani@seloger.com>
2019-05-04 23:44:37 +0200

    Add some validators

---
+++ Synker.Application/DataSources/Queries/GetDatasource/GetDataSourceQueryValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace Synker.Application.DataSources.Queries.GetDatasource
+{
+    using FluentValidation;
+    public class GetDataSourceQueryValidator : AbstractValidator<GetDataSourceQuery>
+    {
+        public GetDataSourceQueryValidator()
+        {
+            RuleFor(x => x.Id).GreaterThan(-1);
+        }
+    }
+}
---
+++ Synker.Application/DataSources/Queries/GetListDatasource/ListDataSourceQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿namespace Synker.Application.DataSources.Queries.GetListDatasource
+{
+    using FluentValidation;
+    using System;
+    public class ListDataSourceQueryValidator : AbstractValidator<ListDatasourceQuery>
+    {
+        public ListDataSourceQueryValidator()
+        {
+            RuleFor(x => x.CreatedFrom).LessThanOrEqualTo(DateTime.UtcNow).When(x => x.CreatedFrom.HasValue);
+        }
+    }
+}
