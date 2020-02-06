06aa92ac1cdba42fcbcb68f06a780964a42011f1
Fazzani <fazzani.heni@outlook.fr>
2020-02-06 17:28:07 +0100

    dotnet core 3.1 migration

---
+++ src/Synker.Api/Startup.cs
@@ -0,0 +1,141 @@
+﻿using AutoMapper;
+using BeatPulse.UI;
+using FluentValidation.AspNetCore;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NSwag;
+using Synker.Api.Filters;
+using Synker.Application.DataSources.Queries.GetDatasource;
+using Synker.Application.Infrastructure.AutoMapper;
+using Synker.Application.Infrastructure.FluentValidationBehaviors;
+using Synker.Application.Interfaces;
+using Synker.Application.Playlists.Commands;
+using Synker.Infrastructure;
+using Synker.Persistence;
+using System;
+using System.Reflection;
+using Xtream.Client;
+
+namespace Synker.Api
+{
+    public class Startup
+    {
+        private volatile static string assemblyVersion = typeof(Startup).GetTypeInfo().Assembly.GetCustomAttribute<AssemblyFileVersionAttribute>().Version;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public static string AssemblyVersion { get => assemblyVersion; set => assemblyVersion = value; }
+
+        // This method gets called by the runtime. Use this method   to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(x=>x.EnableEndpointRouting=false)
+                .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining(typeof(UpdatePlaylistValidator)));
+            services.AddControllers(o => o.Filters.Add(typeof(CustomExceptionFilterAttribute)))
+                .ConfigureApiBehaviorOptions(options =>
+            {
+                options.SuppressConsumesConstraintForFormFileParameters = true;
+                options.SuppressInferBindingSourcesForParameters = true;
+                options.SuppressModelStateInvalidFilter = true;
+                options.SuppressMapClientErrors = true;
+                //options.ClientErrorMapping[404].Link =
+                //    "https://httpstatuses.com/404";
+            });
+
+            services.AddAutoMapper(new Assembly[] { typeof(AutoMapperProfile).GetTypeInfo().Assembly });
+
+            //Some problems with the SynkerDBContext migration
+            //services.AddBeatPulseUI();
+
+            services.AddMediatR(typeof(GetDataSourceQuery).GetTypeInfo().Assembly);
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
+
+            services.AddTransient<INotificationService, NotificationService>();
+
+            services.AddTransient<IDataSourceReaderFactory, DefaultDataSourceReaderFactory>();
+            services.AddTransient<IXtreamClient, XtreamClient>();
+            services.AddTransient<IHttpClientFactory, DefaultHttpClientFactory>();
+            
+            services.AddDbContext<ISynkerDbContext, SynkerDbContext>(options =>
+            {
+                var config = Configuration.GetConnectionString("PlDatabase");
+                options.UseNpgsql(Configuration.GetConnectionString("PlDatabase"),
+                sqlOptions =>
+                {
+                    sqlOptions.MigrationsAssembly(typeof(SynkerDbContext).GetTypeInfo().Assembly.GetName().Name);
+                    //Configuring Connection Resiliency:
+                    sqlOptions.
+                    EnableRetryOnFailure(3, TimeSpan.FromSeconds(30), null);
+                });
+
+                //// Changing default behavior when client evaluation occurs to throw.
+                //// Default in EFCore would be to log warning when client evaluation is done.
+                //options.ConfigureWarnings(warnings => warnings.Throw(
+                //RelationalEventId.QueryClientEvaluationWarning));
+            });
+
+            services.AddApiVersioning(o =>
+            {
+                //o.AssumeDefaultVersionWhenUnspecified = true;
+                o.ReportApiVersions = true;
+                o.DefaultApiVersion = new ApiVersion(1, 0);
+            });
+
+            services.AddOpenApiDocument(config =>
+            {
+                config.PostProcess = document =>
+                {
+                    document.Info = new OpenApiInfo
+                    {
+                        Version = "v1",
+                        Title = "Synker API",
+                        Description = $"Synchronize playlists API {AssemblyVersion}",
+                        TermsOfService = "None",
+                        Contact = new OpenApiContact { Name = "Synker", Email = "contact@synker.ovh", Url = "https://www.github.com/fazzani/synker2" },
+                        License = new OpenApiLicense { Name = "Use under MIT", Url = "" },
+                    };
+                };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            app.UseMvc()
+               .UseApiVersioning()
+               .UseMvcWithDefaultRoute();
+
+            app.UseRouting();
+
+            app.UseBeatPulseUI();
+            app.UseStaticFiles();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllers();
+            //});
+
+            app.UseOpenApi();
+            app.UseSwaggerUi3(settings =>
+            {
+                settings.Path = "/api";
+                //settings.DocumentPath = "/api/specification.json";
+            });
+
+        }
+    }
+}
---
+++ src/Synker.Application/Playlists/Commands/Create/CreatePlaylistCommand.cs
@@ -0,0 +1,60 @@
+﻿using MediatR;
+using Synker.Application.Exceptions;
+using Synker.Application.Interfaces;
+using Synker.Domain.Entities;
+using Synker.Domain.Entities.Core;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Synker.Application.Playlists.Commands
+{
+    public class CreatePlaylistCommand : IRequest<long>
+    {
+        public string Name { get; set; }
+
+        public bool State { get; set; }
+
+        public long? UserId { get; set; }
+
+        public List<Media> Medias { get; }
+
+        public class Handler : IRequestHandler<CreatePlaylistCommand, long>
+        {
+            private readonly ISynkerDbContext _context;
+            private readonly IMediator _mediator;
+
+            public Handler(ISynkerDbContext context, IMediator mediator)
+            {
+                _context = context;
+                _mediator = mediator;
+            }
+
+            public async Task<long> Handle(CreatePlaylistCommand request, CancellationToken cancellationToken)
+            {
+                var user = await _context.Users.FindAsync(new object[] { request.UserId }, cancellationToken: cancellationToken);
+
+                if (user == null)
+                {
+                    throw new NotFoundException(nameof(User), request.UserId);
+                }
+
+                var entity = new Playlist
+                {
+                    Name = request.Name,
+                    State = request.State ? OnlineState.Enabled : OnlineState.Disabled,
+                    User = user
+                };
+
+                entity.AddRangeMedia(request.Medias);
+
+                _context.Playlists.Add(entity);
+                await _context.SaveChangesAsync(cancellationToken);
+
+                await _mediator.Publish(new PlaylistCreated { PlaylistId = entity.Id }, cancellationToken);
+
+                return entity.Id;
+            }
+        }
+    }
+}
